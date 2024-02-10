67c17913946a305a2a9785855a0965c108e5e965
hellwoosh <hellwoosh@gmail.com>
2024-02-11 02:21:03 +0500

    Coursework
    Coursework

---
+++ Solution/SerialKillerPredictionUI/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SerialKillerPredictionUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string _modelPath = string.Empty;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _modelPath = $"{Environment.CurrentDirectory}\\TestModel";
+            ModelPath.Content = _modelPath;
+        }
+
+        private void GetResult_Click(object sender, RoutedEventArgs e)
+        {
+            double[] input =
+            {
+                X1.SelectedIndex,
+                double.Parse(X2.Text),
+                X3.SelectedIndex,
+                X4.SelectedIndex,
+                X5.SelectedIndex,
+                X6.SelectedIndex,
+                X7.SelectedIndex,
+                X8.SelectedIndex,
+                X9.SelectedIndex,
+                double.Parse(X10.Text),
+                X11.SelectedIndex
+            };
+            
+            for (int i  = 0; i < input.Length; i++)
+            {
+                input[i] += 1;
+            }
+
+            Predictor predictor = new(_modelPath);
+            double result = predictor.Predict(input);
+            Result.Content = result;
+            if (result > 0.5)
+            {
+                TextResult.Content = "Является серийным убийцей";
+                TextResult.Foreground = Brushes.Red;
+            }
+            else
+            {
+                TextResult.Content = "Не является серийным убийцей";
+                TextResult.Foreground = Brushes.Green;
+            }
+        }
+
+        private void Label_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            using var dialog = new System.Windows.Forms.FolderBrowserDialog
+            {
+                Description = "Time to select a folder",
+                UseDescriptionForTitle = true,
+                SelectedPath = Environment.CurrentDirectory,
+                ShowNewFolderButton = true
+            };
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                _modelPath = dialog.SelectedPath;
+                ModelPath.Content = _modelPath;
+            }
+        }
+    }
+}
---
+++ Solution/SerialKillerPredictionUI/Predictor.cs
@@ -0,0 +1,47 @@
+﻿using Keras.Models;
+using Numpy;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using System.Windows.Media.Media3D;
+
+namespace SerialKillerPredictionUI
+{
+    class Predictor
+    {
+        private string _modelPath;
+
+        public Predictor(string modelPath)
+        {
+            _modelPath = modelPath;
+        }
+
+        public double Predict(double[] input)
+        {
+            double[] mean = File.ReadAllLines(_modelPath + "\\mean.csv").Select(s => double.Parse(s, CultureInfo.InvariantCulture)).ToArray();
+            double[] std = File.ReadAllLines(_modelPath + "\\std.csv").Select(s => double.Parse(s, CultureInfo.InvariantCulture)).ToArray();
+            var model = Model.LoadModel(_modelPath + "\\model");
+
+            if (model is null) 
+            {
+                throw new System.Exception("Не удалось загрузить модель");
+            }
+
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                input[i] -= mean[i];
+                input[i] /= std[i];
+            }
+
+            var inputNP = np.array(input);
+            var a = inputNP.reshape(new int[] { 1, 11 });
+            var b = inputNP.reshape(new int[] { 11, 1 });
+            var result = model.Predict(a);
+            var rr = result.GetData<double>();
+
+            return (float)rr[0];
+        }
+    }
+}
